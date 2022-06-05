3b53292b0a2d3118ac5327bc2b77114fbdd32fd2
Nick Viscomi <nicholasviscomi@gmail.com>
2022-06-04 20:52:48 -0400

    upgraded the ascii characters used to make it look cooler

--- src/color.go
+++ src/color.go
@@ -1,14 +1,29 @@
 package main
 
 const (
-	Reset  = "\033[0m"
-	Red    = "\033[31m"
-	Green  = "\033[32m"
-	Yellow = "\033[33m"
-	Blue   = "\033[34m"
-	Purple = "\033[35m"
-	Cyan   = "\033[36m"
-	Gray   = "\033[37m"
-	White  = "\033[97m"
-	Bold   = "\033[1m"
+	Reset  = 0
+	Red    = 1
+	Green  = 2
+	Yellow = 3
+	Blue   = 4
+	Purple = 5
+	Cyan   = 6
+	Gray   = 7
+	White  = 8
+	Bold   = 9
+	Underline = 10
 )
+
+var colors = []string{
+	"\033[0m",
+	"\033[31m",
+	"\033[32m",
+	"\033[33m",
+	"\033[34m",
+	"\033[35m",
+	"\033[36m",
+	"\033[37m",
+	"\033[97m",
+	"\033[1m", 	
+	"\u001b[4m",
+}
--- src/tree.go
+++ src/tree.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 )
 
@@ -22,7 +23,8 @@ func main() {
 	var cont  rune
 	numFiles := FullLength(dir, pwd)
 	if numFiles > 100 {
-		fmt.Printf("There are %s%s%d elements%s. Would you like to continue? [y/n]\n", Red, Bold, numFiles, Reset)
+		fmt.Printf("There are %s%s%d elements%s. Would you like to continue? [y/n]\n", 
+						colors[Red], colors[Bold], numFiles, colors[Reset])
 		_,err = fmt.Scan(&cont)
 
 		if err != nil || cont != 'y' {
@@ -31,7 +33,42 @@ func main() {
 		}
 	}
 	
-	PrintDir(dir, 0, pwd)
+	PrintDir(dir, 0, pwd, 10)
+}
+
+/*
+dir = the directory being listed out
+err = the return value from reading a directory
+level = how many subdirectories have been explored. 
+	--> Used to format the output with tabs
+*/
+func PrintDir(dir []os.DirEntry, level int, pwd string, parentColor int) {
+	for _,entry := range dir {
+		for i := 0; i < level; i++ {
+			fmt.Print("  ")
+		}
+		if entry.Name() == ".DS_Store" { continue }
+		if entry.Name() == ".git" { continue }
+
+		currColorInd := rand.Intn(7) + 2
+		if entry.IsDir() {
+			//Exit point for recursion
+			fmt.Printf("%s%s└──%s\n%s", 
+			colors[Bold], colors[currColorInd], entry.Name(), colors[Reset])
+
+			path := pwd + "/" + entry.Name()
+			subdir,err := os.ReadDir(path)
+			if err != nil {
+				fmt.Println("printdir: error reading sub directory!")
+			}
+
+			PrintDir(subdir, level + 1, path, Green)
+			continue
+		}
+
+
+		fmt.Printf("├──%s\n", entry.Name())
+	}
 }
 
 var count = 0
@@ -54,37 +91,4 @@ func FullLength(dir []os.DirEntry, pwd string) int {
 		count++
 	}
 	return count
-}
-
-/*
-dir = the directory being listed out
-err = the return value from reading a directory
-level = how many subdirectories have been explored. 
-	--> Used to format the output with tabs
-*/
-func PrintDir(dir []os.DirEntry, level int, pwd string) {
-	for _,entry := range dir {
-		for i := 0; i < level; i++ {
-			fmt.Print("  ")
-		}
-		if entry.Name() == ".DS_Store" { continue }
-		if entry.Name() == ".git" { continue }
-
-		if entry.IsDir() {
-			//Exit point for recursion
-			fmt.Printf("%s%s——> %s\n%s", Bold, Green, entry.Name(), Reset)
-
-			path := pwd + "/" + entry.Name()
-			subdir,err := os.ReadDir(path)
-			if err != nil {
-				fmt.Println("printdir: error reading sub directory!")
-			}
-
-			PrintDir(subdir, level + 1, path)
-			continue
-		}
-
-
-		fmt.Printf("——> %s\n", entry.Name())
-	}
 }
